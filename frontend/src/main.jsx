fd3130eda86d3f9a3ef20db3d84121472d75169f
sgnawali25 <sandeshgyawali25@gmail.com>
2025-11-14 14:01:15 -0600

    updated google id

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,6 @@ import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const id = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID;
-console.log("Google Client ID:", id);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={id}>
